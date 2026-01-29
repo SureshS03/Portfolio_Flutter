9758f23252bcfb86aee4bb09c6ce6eab0694cae7
Suresh S <suthamani51@gmail.com>
2026-01-29 23:00:42 +0530

    rename title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Suresh Portfolio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
